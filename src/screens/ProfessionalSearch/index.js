734543fccc67849963d5b62df9861645fe1ea1c7
BrunoBersan <brunobersan13@gmail.com>
2019-05-28 15:45:17 -0300

    BRUNO

---
+++ src/screens/ProfessionalSearch/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import Container from '../Container/index'
+
+export default function ProfessionalSearch(props) {
+    return (
+        <React.Fragment>
+        <Container />
+        <View>
+            teste
+        </View>
+        </React.Fragment> 
+    )
+}
+
+ProfessionalSearch.navigationOptions = {
+    header: null
+}

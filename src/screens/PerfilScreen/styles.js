6e96af4212153b277f70e7c76aad27088bb4f2bd
Felipe <felipe.lima.flr@gmail.com>
2019-05-30 11:43:37 -0300

    ajuste perfil

--- src/screens/PerfilScreen/styles.js
+++ src/screens/PerfilScreen/styles.js
@@ -41,7 +41,7 @@ export const ContainerAvatar = styled.View`
     right: 0;  
     left: 0;
     align-items: center;
-    top: ${actualDimensions.height >= 720 ? 250 : 40};
+    top: ${(actualDimensions.height * 40) / 692};
 `
 export const styles = StyleSheet.create({
     shadow: {

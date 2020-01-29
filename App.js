5c62d98a7b8e2ec3c7e40e49a7650267e2fa1b59
Felipe Lima <felipe.lima.flr@gmail.com>
2020-01-29 14:07:21 -0300

    Ajuste Navegação

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Easing, Animated } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 
@@ -36,7 +37,31 @@ const MainNavigator = createStackNavigator(
     ClientListChat: { screen: ClientListChatScreen }
   },
   {
-    initialRouteName: 'Splash'
+    initialRouteName: 'Splash',
+    transitionConfig: () => ({
+      transitionSpec: {
+        duration: 300,
+        easing: Easing.out(Easing.poly(4)),
+        timing: Animated.timing,
+      },
+      screenInterpolator: sceneProps => {
+        const { layout, position, scene } = sceneProps;
+        const { index } = scene;
+
+        const width = layout.initWidth;
+        const translateX = position.interpolate({
+          inputRange: [index - 1, index, index + 1],
+          outputRange: [width, 0, 0],
+        });
+
+        const opacity = position.interpolate({
+          inputRange: [index - 1, index - 0.99, index],
+          outputRange: [0, 1, 1],
+        });
+
+        return { opacity, transform: [{ translateX: translateX }] };
+      },
+    })
   }
 );
 
